4955b0299e7f28dd2260f5902baa92af1a74a6a8
Grady Weyenberg <gradysw@hawaii.edu>
2020-04-29 00:39:48 -1000

    undo, redo, highlighter, axes tools

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,4 @@
-var shapes = [];
-var pointerMap = {};
-
+let mementos = [];
 
 $(document).ready(init);
 
@@ -13,11 +11,13 @@ function init() {
         togglePaletteMoreText: ">",
         togglePaletteLessText: "<",
         hideAfterPaletteSelect: true,
-        palette: [["black", "red"],
-        ["mediumseagreen", "mediumblue"],
-        ["mediumorchid", "darkorange"]],
+        palette: [
+            ["black", "red"],
+            ["mediumseagreen", "mediumblue"],
+            ["mediumorchid", "darkorange"]
+        ],
         replacerClassName: "sp-replacer btn btn-secondary bg-secondary",
-        change: color => project.currentStyle.strokeColor = color.toHexString()
+        change: color => project.currentStyle.strokeColor.set(color.toRgbString())
     });
 
     $("#clearButton").click(event => project.clear());
@@ -25,7 +25,8 @@ function init() {
     $("input[name=tool]").click(event => tools.find(tool => tool.name == event.target.value).activate());
 
     $("#sizeSlider").change(event => project.currentStyle.strokeWidth = event.target.value);
-
+    $("#undoButton").click(undo);
+    $("#redoButton").click(redo);
     project.currentStyle.strokeColor = $("#colorPicker").spectrum("get").toHexString();
     project.currentStyle.strokeWidth = $("#sizeSlider").val();
     project.currentStyle.strokeCap = "round";
@@ -72,7 +73,7 @@ const eraseTool = new paper.Tool({
     name: "erase",
     path: null,
     onMouseDown: function (event) {
-        eraseTool.path = new Path({ strokeWidth: project.currentStyle.strokeWidth * 3, strokeColor: "white"});
+        eraseTool.path = new Path({ strokeWidth: project.currentStyle.strokeWidth * 3, strokeColor: "white" });
     },
     onMouseDrag: function (event) {
         if (eraseTool.path) eraseTool.path.add(event.point);
@@ -90,9 +91,6 @@ const circleTool = new Tool({
     },
     onMouseDrag: function (event) {
         circleTool.path.radius = (event.point - event.downPoint).length;
-        // let path = circleTool.path;
-        // let radius = (event.point - event.downPoint).length;
-        // if (radius > 1) path.scale(radius / (path.firstSegment.point - path.bounds.center).length);
     },
     onMouseUp: function (event) {
         circleTool.path = null;
@@ -103,20 +101,87 @@ const lineTool = new Tool({
     name: "line",
     path: null,
     onMouseDown: event => lineTool.path = new Path([event.point, event.point]),
-    onMouseDrag: function(event){
+    onMouseDrag: function (event) {
         lineTool.path.lastSegment.point.set(event.point);
-        lineTool.path.firstSegment.point.set(event.modifiers.shift ? event.downPoint - (event.point - event.downPoint): event.downPoint);
+        if (event.modifiers.shift) lineTool.path.firstSegment.point.set(lineMirror(event));
+        lineTool.path.firstSegment.point.set(event.modifiers.shift ? lineMirror(event) : event.downPoint);
     },
     onMouseUp: event => lineTool.path = null
 });
+
+function lineMirror(event) {
+    return event.downPoint - (event.point - event.downPoint);
+}
 
 const rectTool = new Tool({
     name: "rectangle",
     path: null,
     onMouseDown: event => rectTool.path = new Shape.Rectangle(event.point, event.point),
-    onMouseDrag: function(event){
-        rectTool.path.size.set(event.point - event.downPoint);
-        if(!event.modifiers.shift) rectTool.path.position.set((event.downPoint+event.point)/2);
+    onMouseDrag: function (event) {
+        let size = event.point - event.downPoint;
+        if (event.modifiers.shift) {
+            rectTool.path.size.set(size * 2);
+            rectTool.path.position.set(event.downPoint);
+        }
+        else {
+            rectTool.path.size.set(size);
+            rectTool.path.position.set((event.downPoint + event.point) / 2);
+        }
     },
-    onMouseUp: event => {rectTool.path=null}
+    onMouseUp: event => { rectTool.path = null }
 });
+
+const highlightTool = new Tool({
+    name: "highlight",
+    path: null,
+    onMouseDown: function (event) {
+        highlightTool.path = new Path({ strokeWidth: project.currentStyle.strokeWidth * 5 });
+        highlightTool.path.strokeColor.alpha = 0.4;
+
+    },
+    onMouseDrag: event => highlightTool.path.add(event.point),
+    onMouseUp: event => highlightTool.path = null
+});
+
+const axesTool = new Tool({
+    name: "axes",
+    path: null,
+    onMouseDown: function (event) {
+        axesTool.path = new CompoundPath({
+            children: [
+                new Path([event.point, event.point]),
+                new Path([event.point, event.point])
+            ]
+        })
+    },
+    onMouseDrag: function (event) {
+        let xaxis = axesTool.path.firstChild;
+        let yaxis = axesTool.path.lastChild;
+        let mirror = lineMirror(event);
+        xaxis.firstSegment.point.x = event.point.x;
+        xaxis.lastSegment.point.x = event.modifiers.shift ? mirror.x : event.downPoint.x;
+        yaxis.firstSegment.point.y = event.point.y;
+        yaxis.lastSegment.point.y = event.modifiers.shift ? mirror.y : event.downPoint.y;
+    },
+    onMouseUp: event => axesTool.path = null
+});
+
+function undo(event) {
+    let item = project.activeLayer.lastChild;
+    if (item) {
+        mementos.push(item);
+        item.remove();
+        $("#redoButton").removeAttr("disabled");
+    }
+}
+
+function redo(event) {
+    let item = mementos.pop();
+    if (mementos.length == 0) $("#redoButton").prop('disabled', true);
+    if (item) project.activeLayer.addChild(item);
+}
+
+function clearUndo() {
+    mementos.length = 0;
+    $("#redoButton").prop('disabled', true);
+}
