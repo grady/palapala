e6134a99bb0dd078111f24c62ea3d984debc8c6c
Grady Weyenberg <gradysw@hawaii.edu>
2020-05-12 19:39:42 -1000

    use MemoryDB if MONGO_URL env var not set

--- js/server.js
+++ js/server.js
@@ -5,23 +5,28 @@ const shortid = require('shortid');
 
 app.use("/:id/", express.static(path.join(__dirname, "..")));
 app.use("/:id/view/", express.static(path.join(__dirname, "..")));
-app.use("/", (req,res) => {
+app.use("/", (req, res) => {
     res.redirect("/" + shortid.generate());
 });
 
 const server = app.listen(process.env.PORT || 8000);
 const ShareDB = require('sharedb');
-const mongodb = require("mongodb");
-const db = require("sharedb-mongo")({
-    mongo: cb => mongodb.connect(process.env.MONGO_URL, {useUnifiedTopology: true}, cb)
-});
-const backend = new ShareDB({db});
+let db;
+if (process.env.MONGO_URL) {
+    const mongodb = require("mongodb");
+    db = require("sharedb-mongo")({
+        mongo: cb => mongodb.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, cb)
+    });
+} else {
+    db = ShareDB.MemoryDB();
+}
+const backend = new ShareDB({ db });
 
 const WebSocket = require('ws');
 const WebSocketJSONStream = require('websocket-json-stream');
-const wss = new WebSocket.Server({server: server});
+const wss = new WebSocket.Server({ server: server });
 
-wss.on("connection", function(ws, req){
+wss.on("connection", function (ws, req) {
     console.log("connection acccepted: " + (req.headers['x-forwarded-for'] || req.connection.remoteAddress) + req.url);
     let stream = new WebSocketJSONStream(ws);
     backend.listen(stream);
